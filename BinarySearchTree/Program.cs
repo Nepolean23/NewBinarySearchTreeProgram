f9378d954abcb7337385750c8533e85369649a0f
Nepolean23 <nlean1996@gmail.com>
2023-01-23 07:23:10 +0530

    [NEPOLEAN]-Code for Checking Of an Added Values

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,7 +1,23 @@
 ﻿using BinarySearchTree;
 
 //Adding of values to the BinarySearchTree
-CreatingBinarySearchTree<int> binarySearch = new CreatingBinarySearchTree<int>(56);
+//CreatingBinarySearchTree<int> binarySearch = new CreatingBinarySearchTree<int>(56);
+//binarySearch.Insert(30);
+//binarySearch.Insert(70);
+//binarySearch.Insert(22);
+//binarySearch.Insert(40);
+//binarySearch.Insert(60);
+//binarySearch.Insert(95);
+//binarySearch.Insert(11);
+//binarySearch.Insert(65);
+//binarySearch.Insert(3);
+//binarySearch.Insert(16);
+//binarySearch.Insert(63);
+//binarySearch.Insert(67);
+//binarySearch.Display();
+
+//Checking of values in BinarySearchTree
+CheckingValuesInBinarySearchTree<int> binarySearch = new CheckingValuesInBinarySearchTree<int>(56);
 binarySearch.Insert(30);
 binarySearch.Insert(70);
 binarySearch.Insert(22);
